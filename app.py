38f53c6ae01ca33eff70abd5b79a4ea06835c9f0
ferchirinoboss <chirinoboss@gmail.com>
2024-11-24 14:10:42 -0600

    Proxy for cover img implemented

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from flask import Flask, redirect, render_template, request, session
+from flask import Flask, redirect, render_template, request, session, Response
 import requests
 
 app = Flask(__name__)
 
-def add_manga(list, id, title, cover_art_id, cover_fileName):
+def add_manga(list, id, title, cover_art_id, cover_fileName, cover_img):
     list.append({
         "id": id,
         "title": title,
         "cover_art_id": cover_art_id,
         "cover_fileName": cover_fileName,
-        "cover_img": f"https://uploads.mangadex.org/covers/{id}/{cover_fileName}" 
+        "cover_img": cover_img 
     })
 
 @app.route('/')
@@ -20,6 +20,16 @@ def index():
 @app.route('/library')
 def library():
     return render_template("library.html")
+
+@app.route('/proxy-cover')    # PROXY FOR GETTING THE COVER IMG 
+def proxy():
+    cover_url = request.args.get('url')
+    response = requests.get(cover_url, stream=True)
+    if response.status_code == 200:
+        return Response(response.content, content_type=response.headers['Content-Type'])
+    else:
+        return Response(f"Error fetching image: {response.status_code}", status=response.status_code)
+    
 
 @app.route('/search', methods=["GET", "POST"])
 def search():
@@ -43,13 +53,19 @@ def search():
                     cover_art_id = relationship["id"]
 
 
-            cover_fileName_response = requests.get(
-                f"{base_url}/cover/{cover_art_id}"
-                # params={"id": cover_art_id}
-            )
-            cover_fileName = cover_fileName_response.json()["data"]["attributes"]["fileName"]
+                    cover_fileName_response = requests.get(
+                        f"{base_url}/cover/{cover_art_id}"
+                        # params={"id": cover_art_id}
+                    )
+                    cover_fileName = cover_fileName_response.json()["data"]["attributes"]["fileName"]
+            
+            if cover_fileName:
+                cover_url = f"https://uploads.mangadex.org/covers/{manga_id}/{cover_fileName}"  
 
-            add_manga(manga_collection, manga_id, manga_title, cover_art_id, cover_fileName)
+                # use the proxy route 
+                proxy_cover_url = f"/proxy-cover?url={cover_url}"
+
+            add_manga(manga_collection, manga_id, manga_title, cover_art_id, cover_fileName, proxy_cover_url)
             
 
         return render_template("search.html", manga_collection=manga_collection, cover_fileName_response=cover_fileName_response)
